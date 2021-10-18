6ed372de6d907cbd41d61cb6cbd69d11dfc8404c
marina <marina.chompalova@ft.com>
2021-10-18 15:42:54 +0300

    Migrate to cmneo4j driver

--- content/content_service.go
+++ content/content_service.go
@@ -2,12 +2,10 @@ package content
 
 import (
 	"encoding/json"
+	"errors"
 	"time"
 
-	"github.com/Financial-Times/go-logger"
-	"github.com/Financial-Times/neo-utils-go/neoutils"
-	tid "github.com/Financial-Times/transactionid-utils-go"
-	"github.com/jmcvetta/neoism"
+	cmneo4j "github.com/Financial-Times/cm-neo4j-driver"
 )
 
 const LiveBlogPackage = "LiveBlogPackage"
@@ -24,42 +22,38 @@ var contentTypesWithNoBody = map[string]bool{
 	LiveBlogPost:     true,
 }
 
-// CypherDriver - CypherDriver
-type service struct {
-	conn neoutils.NeoConnection
+type Service struct {
+	driver *cmneo4j.Driver
 }
 
 //NewCypherDriver instantiate driver
-func NewCypherContentService(cypherRunner neoutils.NeoConnection) service {
-	return service{cypherRunner}
+func NewContentService(d *cmneo4j.Driver) Service {
+	return Service{
+		driver: d,
+	}
 }
 
-//Initialise ensure constraint on content uuid
-func (cd service) Initialise() error {
+//Initialise ensures constraints on content uuid
+func (cd Service) Initialise() error {
 
-	return cd.conn.EnsureConstraints(map[string]string{
+	return cd.driver.EnsureConstraints(map[string]string{
 		"Content": "uuid"})
 }
 
 // Check - Feeds into the Healthcheck and checks whether we can connect to Neo and that the datastore isn't empty and
 // also checks if we are connected to leader/writable node
-func (cd service) Check() error {
-	writableErr := neoutils.CheckWritable(cd.conn)
-	if writableErr != nil {
-		return writableErr
-	}
-
-	return neoutils.Check(cd.conn)
+func (cd Service) Check() error {
+	return cd.driver.VerifyConnectivity()
 }
 
 // Read - reads a content given a UUID
-func (cd service) Read(uuid string, transId string) (interface{}, bool, error) {
+func (cd Service) Read(uuid string, transId string) (interface{}, bool, error) {
 	var results []struct {
 		content
 	}
 
-	query := &neoism.CypherQuery{
-		Statement: `MATCH (n:Content {uuid:{uuid}})
+	query := &cmneo4j.Query{
+		Cypher: `MATCH (n:Content {uuid:{uuid}})
 			OPTIONAL MATCH (sp:Thing)-[rel1:IS_CURATED_FOR]->(n)
 			OPTIONAL MATCH (n)-[rel2:CONTAINS]->(cp:Thing)
 			WITH n,sp,cp
@@ -68,20 +62,20 @@ func (cd service) Read(uuid string, transId string) (interface{}, bool, error) {
 				n.publishedDate as publishedDate,
 				sp.uuid as storyPackage,
 				cp.uuid as contentPackage`,
-		Parameters: map[string]interface{}{
+		Params: map[string]interface{}{
 			"uuid": uuid,
 		},
 		Result: &results,
 	}
 
-	err := cd.conn.CypherBatch([]*neoism.CypherQuery{query})
+	err := cd.driver.Read(query)
 
-	if err != nil {
+	if errors.Is(err, cmneo4j.ErrNoResultsFound) {
 		return content{}, false, err
 	}
 
-	if len(results) == 0 {
-		return content{}, false, nil
+	if err != nil {
+		return content{}, false, err
 	}
 
 	result := results[0]
@@ -97,13 +91,11 @@ func (cd service) Read(uuid string, transId string) (interface{}, bool, error) {
 }
 
 //Write - Writes a content node
-func (cd service) Write(thing interface{}, transId string) error {
+func (cd Service) Write(thing interface{}, transId string) error {
 	c := thing.(content)
 
 	// Letting through only articles (which have body), live blogs, content packages, graphics, videos and audios (which don't have a body)
 	if c.Body == "" && !contentTypesWithNoBody[c.Type] {
-		logger.WithField(tid.TransactionIDKey, transId).
-			Infof("There is no body with this content item therefore assuming is it not an Article: %v", c.UUID)
 		return nil
 	}
 
@@ -127,30 +119,26 @@ func (cd service) Write(thing interface{}, transId string) error {
 		params["publishedDateEpoch"] = datetimeEpoch.Unix()
 	}
 
-	deleteEntityRelationshipsQuery := &neoism.CypherQuery{
-		Statement: `MATCH (t:Thing {uuid:{uuid}})
+	deleteEntityRelationshipsQuery := &cmneo4j.Query{
+		Cypher: `MATCH (t:Thing {uuid:{uuid}})
 				OPTIONAL MATCH (c:Thing)-[rel1:IS_CURATED_FOR]->(t)
 				OPTIONAL MATCH (cp:Thing)<-[rel2:CONTAINS]-(t)
 				DELETE rel1, rel2`,
-		Parameters: map[string]interface{}{
+		Params: map[string]interface{}{
 			"uuid": c.UUID,
 		},
 	}
 
-	queries := []*neoism.CypherQuery{deleteEntityRelationshipsQuery}
+	queries := []*cmneo4j.Query{deleteEntityRelationshipsQuery}
 
 	labels := `:Content`
 
 	if c.StoryPackage != "" {
-		logger.WithField(tid.TransactionIDKey, transId).
-			Infof("There is a story package with uuid=%v attached to Article with uuid=%v", c.StoryPackage, c.UUID)
 		addStoryPackageRelationQuery := addStoryPackageRelationQuery(c.UUID, c.StoryPackage)
 		queries = append(queries, addStoryPackageRelationQuery)
 	}
 
 	if c.ContentPackage != "" {
-		logger.WithField(tid.TransactionIDKey, transId).
-			Infof("There is a content package with uuid=%v attached to Article with uuid=%v", c.ContentPackage, c.UUID)
 		addContentPackageRelationQuery := addContentPackageRelationQuery(c.UUID, c.ContentPackage)
 		queries = append(queries, addContentPackageRelationQuery)
 
@@ -160,60 +148,56 @@ func (cd service) Write(thing interface{}, transId string) error {
 		}
 	}
 
-	statement := `MERGE (n:Thing {uuid: {uuid}})
+	query := `MERGE (n:Thing {uuid: {uuid}})
 		      set n={allprops}
 		      set n ` + labels
 
-	writeContentQuery := &neoism.CypherQuery{
-		Statement: statement,
-		Parameters: map[string]interface{}{
+	writeContentQuery := &cmneo4j.Query{
+		Cypher: query,
+		Params: map[string]interface{}{
 			"uuid":     c.UUID,
 			"allprops": params,
 		},
 	}
 
 	queries = append(queries, writeContentQuery)
-	err := cd.conn.CypherBatch(queries)
+	err := cd.driver.Write(queries...)
 	if err != nil {
-		logger.WithMonitoringEvent("SaveNeo4j", transId, c.Type).WithField("uuid", c.UUID).WithError(err).Errorf("error: the query could not be executed")
-	} else {
-		logger.WithMonitoringEvent("SaveNeo4j", transId, c.Type).WithField("uuid", c.UUID).Info("the query was successfully executed")
+		return err
 	}
-	return err
+	return nil
 }
 
-func addStoryPackageRelationQuery(articleUUID, packageUUID string) *neoism.CypherQuery {
-	statement := `MERGE(sp:Thing{uuid:{packageUuid}})
+func addStoryPackageRelationQuery(articleUUID, packageUUID string) *cmneo4j.Query {
+	query := `MERGE(sp:Thing{uuid:{packageUuid}})
 			MERGE(c:Thing{uuid:{contentUuid}})
 			MERGE(c)<-[rel:IS_CURATED_FOR]-(sp)`
 
-	query := &neoism.CypherQuery{
-		Statement: statement,
-		Parameters: map[string]interface{}{
+	return &cmneo4j.Query{
+		Cypher: query,
+		Params: map[string]interface{}{
 			"packageUuid": packageUUID,
 			"contentUuid": articleUUID,
 		},
 	}
-	return query
 }
 
-func addContentPackageRelationQuery(articleUUID, packageUUID string) *neoism.CypherQuery {
-	statement := `MERGE(cp:Thing{uuid:{packageUuid}})
+func addContentPackageRelationQuery(articleUUID, packageUUID string) *cmneo4j.Query {
+	query := `MERGE(cp:Thing{uuid:{packageUuid}})
 			MERGE(c:Thing{uuid:{contentUuid}})
 			MERGE(c)-[rel:CONTAINS]->(cp)`
 
-	query := &neoism.CypherQuery{
-		Statement: statement,
-		Parameters: map[string]interface{}{
+	return &cmneo4j.Query{
+		Cypher: query,
+		Params: map[string]interface{}{
 			"packageUuid": packageUUID,
 			"contentUuid": articleUUID,
 		},
 	}
-	return query
 }
 
-//Delete - Deletes a content
-func (cd service) Delete(uuid string, transId string) (bool, error) {
+//Delete - Deletes a content item
+func (cd Service) Delete(uuid string, transId string) (bool, error) {
 	// "clearCollectionNode" query handles a specific case when
 	// a Content Collection was deleted, which means its contents are removed
 	// and the "ContentCollection" label was removed, but the node remains in Neo4j
@@ -222,79 +206,74 @@ func (cd service) Delete(uuid string, transId string) (bool, error) {
 	// the related hanging node gets deleted by this query.
 
 	// Check "content-collection-rw-neo4j" service for the the Content Collection deletion query.
-	clearCollectionNode := &neoism.CypherQuery{
-		Statement: `
+	clearCollectionNode := &cmneo4j.Query{
+		Cypher: `
 			MATCH (p:ContentPackage {uuid: {uuid}})-[rel:CONTAINS]->(cc:Thing)
 			OPTIONAL MATCH (cc)-[rel]-()
 			WITH cc, count(rel) AS relCount
 			WHERE relCount = 1 AND NOT cc:ContentCollection
 			DETACH DELETE cc
 		`,
-		Parameters: map[string]interface{}{
+		Params: map[string]interface{}{
 			"uuid": uuid,
 		},
 	}
 
-	removeNode := &neoism.CypherQuery{
-		Statement: `
+	removeNode := &cmneo4j.Query{
+		Cypher: `
 			MATCH (p:Thing {uuid: {uuid}})
 			DETACH DELETE p
 		`,
-		Parameters: map[string]interface{}{
+		Params: map[string]interface{}{
 			"uuid": uuid,
 		},
-		IncludeStats: true,
+		IncludeSummary: true,
 	}
 
-	err := cd.conn.CypherBatch([]*neoism.CypherQuery{clearCollectionNode})
+	err := cd.driver.Write(clearCollectionNode)
 	if err != nil {
-		logger.WithMonitoringEvent("SaveNeo4j", transId, "").WithField("uuid", uuid).WithError(err).Error("error: the extra delete query could not be executed")
 		return false, err
 	}
 	// The queries should be executed in the specified order but `CypherBatch` does not guarantee order,
 	// so we execute them in separate batches
 	// dependency: if a CP is deleted before the first query is executed, there is no way to find the related node
 	// left after a ContentCollections is deleted
-	err = cd.conn.CypherBatch([]*neoism.CypherQuery{removeNode})
-	if err != nil {
-		logger.WithMonitoringEvent("SaveNeo4j", transId, "").WithField("uuid", uuid).WithError(err).Error("error: the delete query could not be executed")
-		return false, err
-	}
-
-	s1, err := removeNode.Stats()
+	err = cd.driver.Write(removeNode)
 	if err != nil {
 		return false, err
 	}
 
-	var deleted bool
-	if s1.NodesDeleted > 0 {
-		deleted = true
+	s1, err := removeNode.Summary()
+	if err != nil {
+		return false, err
 	}
-
-	logger.WithMonitoringEvent("SaveNeo4j", transId, "").WithField("uuid", uuid).Info("the delete query was successfully executed")
-	return deleted, err
+	return s1.Counters().NodesDeleted() > 0, nil
 }
 
 // DecodeJSON - Decodes JSON into content
-func (cd service) DecodeJSON(dec *json.Decoder) (interface{}, string, error) {
+func (cd Service) DecodeJSON(dec *json.Decoder) (interface{}, string, error) {
 	c := content{}
 	err := dec.Decode(&c)
 	return c, c.UUID, err
 }
 
-// Count - Returns a count of the number of content in this Neo instance
-func (cd service) Count() (int, error) {
+// Count - Returns a count of the number of content items in this Neo instance
+func (cd Service) Count() (int, error) {
 
 	var results []struct {
 		Count int `json:"c"`
 	}
 
-	query := &neoism.CypherQuery{
-		Statement: `MATCH (n:Content) return count(n) as c`,
-		Result:    &results,
+	query := &cmneo4j.Query{
+		Cypher: `MATCH (n:Content) return count(n) as c`,
+		Result: &results,
 	}
 
-	err := cd.conn.CypherBatch([]*neoism.CypherQuery{query})
+	err := cd.driver.Read(query)
+
+	if errors.Is(err, cmneo4j.ErrNoResultsFound) {
+		return 0, nil
+	}
 
 	if err != nil {
 		return 0, err
--- main.go
+++ main.go
@@ -3,45 +3,59 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	_ "net/http/pprof"
 	"os"
-
 	"time"
 
 	"github.com/Financial-Times/base-ft-rw-app-go/baseftrwapp"
+	cmneo4j "github.com/Financial-Times/cm-neo4j-driver"
 	"github.com/Financial-Times/content-rw-neo4j/v3/content"
 	fthealth "github.com/Financial-Times/go-fthealth/v1_1"
-	"github.com/Financial-Times/go-logger"
-	"github.com/Financial-Times/neo-utils-go/neoutils"
-	"github.com/jawher/mow.cli"
+	"github.com/Financial-Times/go-logger/v2"
+	cli "github.com/jawher/mow.cli"
 )
 
 const (
-	appName        = "content-rw-neo4j"
-	appDescription = "A RESTful API for managing Content (bare bones representation as full content is served from MongoDB) in neo4j"
+	appDescription = "A RESTful API for managing content (barebone representation as full content is served from MongoDB) in Neo4j"
 )
 
 func main() {
+	app := cli.App("content-rw-neo4j", appDescription)
 
-	app := cli.App(appName, appDescription)
+	appName := app.String(cli.StringOpt{
+		Name:   "app-name",
+		Value:  "content-rw-neo4j",
+		Desc:   "Name of the application",
+		EnvVar: "APP_NAME",
+	})
+
+	appSystemCode := app.String(cli.StringOpt{
+		Name:   "app-system-code",
+		Value:  "upp-content-rw-neo4j",
+		Desc:   "System code of the application",
+		EnvVar: "APP_SYSTEM_CODE",
+	})
+
 	neoURL := app.String(cli.StringOpt{
 		Name:   "neo-url",
 		Value:  "http://localhost:7474/db/data",
 		Desc:   "neo4j endpoint URL",
 		EnvVar: "NEO_URL",
 	})
+
 	apiYml := app.String(cli.StringOpt{
 		Name:   "api-yml",
 		Value:  "./api.yml",
 		Desc:   "Location of the API Swagger YML file.",
 		EnvVar: "API_YML",
 	})
+
 	port := app.Int(cli.IntOpt{
 		Name:   "port",
 		Value:  8080,
 		Desc:   "Port to listen on",
 		EnvVar: "APP_PORT",
 	})
+
 	batchSize := app.Int(cli.IntOpt{
 		Name:   "batchSize",
 		Value:  1024,
@@ -49,26 +63,25 @@ func main() {
 		EnvVar: "BATCH_SIZE",
 	})
 
-	logLevel := app.String(cli.StringOpt{
-		Name:   "logLevel",
-		Value:  "INFO",
-		Desc:   "Logging level (DEBUG, INFO, WARN, ERROR)",
-		EnvVar: "LOG_LEVEL",
-	})
-
-	logger.InitLogger(appName, *logLevel)
+	log := logger.NewUPPInfoLogger(*appName)
+	log.WithFields(map[string]interface{}{
+		"appName":       *appName,
+		"appSystemCode": *appSystemCode,
+		"neoURL":        *neoURL,
+		"port":          *port,
+		"batchSize":     *batchSize,
+	}).Info("Application starting...")
 
 	app.Action = func() {
-		logger.Infof("Application started with args %s", os.Args)
+		log.Info("Application started with args %s", os.Args)
 
-		conf := neoutils.DefaultConnectionConfig()
-		conf.BatchSize = *batchSize
-		db, err := neoutils.Connect(*neoURL, conf)
+		driver, err := cmneo4j.NewDefaultDriver(*neoURL, log)
 		if err != nil {
-			logger.Errorf("Could not connect to neo4j, error=[%s]\n", err)
+			log.WithError(err).Fatal("Could not create a new instance of cmneo4j driver")
 		}
+		defer driver.Close()
 
-		contentDriver := content.NewCypherContentService(db)
+		contentDriver := content.NewContentService(driver)
 		contentDriver.Initialise()
 
 		services := map[string]baseftrwapp.Service{
@@ -77,12 +90,12 @@ func main() {
 
 		var checks []fthealth.Check
 		for _, service := range services {
-			checks = append(checks, makeCheck(service, db))
+			checks = append(checks, makeCheck(service, driver))
 		}
 
 		ymlBytes, err := ioutil.ReadFile(*apiYml)
 		if err != nil {
-			logger.WithField("api-yml", *apiYml).Warn("Failed to read OpenAPI yml file, please confirm the file exists and is not empty.")
+			//logger.WithField("api-yml", *apiYml).Warn("Failed to read OpenAPI yml file, please confirm the file exists and is not empty.")
 			ymlBytes = nil // the base-ft-rw-app-go lib will not add the /__api endpoint if OpenAPIData is nil
 		}
 
@@ -108,17 +121,17 @@ func main() {
 	}
 	err := app.Run(os.Args)
 	if err != nil {
-		logger.Errorf("Application could not start, error=[%s]\n", err)
+		log.WithError(err).Fatalf("Application could not start, error=[%s]\n", err)
 	}
 }
 
-func makeCheck(service baseftrwapp.Service, cr neoutils.CypherRunner) fthealth.Check {
+func makeCheck(service baseftrwapp.Service, cd *cmneo4j.Driver) fthealth.Check {
 	return fthealth.Check{
 		BusinessImpact:   "Cannot read/write content via this writer",
 		Name:             "Check connectivity to Neo4j",
 		PanicGuide:       "https://runbooks.in.ft.com/upp-content-rw-neo4j",
 		Severity:         1,
-		TechnicalSummary: fmt.Sprintf("Cannot connect to Neo4j instance %s with something written to it", cr),
+		TechnicalSummary: fmt.Sprintf("Cannot connect to Neo4j instance %s with something written to it", cd),
 		Checker:          func() (string, error) { return "", service.Check() },
 	}
 }
